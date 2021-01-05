b45ceecb3472112120ade45db5a2ed9f0f9c3235
pashakiz <pashakiz@gmail.com>
2021-01-05 21:13:25 +0300

    add html/css/js

---
+++ src/main/assets/www/precache-manifest.5ca9049a325c444fbfbaf161a8f0efa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac6d7e56ebd7533c987d148fd78de83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c79936e32fc830a1dc45",
+    "url": "/static/css/main.62c408b8.chunk.css"
+  },
+  {
+    "revision": "c35e709252bd293f540c",
+    "url": "/static/js/2.911f9d65.chunk.js"
+  },
+  {
+    "revision": "350916d322e6b4ff7b00295100d999cb",
+    "url": "/static/js/2.911f9d65.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c79936e32fc830a1dc45",
+    "url": "/static/js/main.da1ba831.chunk.js"
+  },
+  {
+    "revision": "e738b54ffe3b15816cf3",
+    "url": "/static/js/runtime-main.63c1efab.js"
+  }
+]);
