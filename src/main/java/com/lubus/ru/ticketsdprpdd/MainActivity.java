86647be6618cc7b333776a0eebf5f3a5e90d9b63
pashakiz <pashakiz@gmail.com>
2021-01-05 22:48:15 +0300

    WebView works with JS and localstorage

--- src/main/java/com/lubus/ru/ticketsdprpdd/MainActivity.java
+++ src/main/java/com/lubus/ru/ticketsdprpdd/MainActivity.java
@@ -1,9 +1,15 @@
 package com.lubus.ru.ticketsdprpdd;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,14 +18,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //init
-        WebView myWebApp = (WebView) findViewById(R.id.webview);
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
 
-        //settings
-        //myWebApp.getSettings().setJavaScriptEnabled(true);
+        WebView webView = (WebView) findViewById(R.id.webview);
 
-        //load
-        myWebApp.loadUrl("http://avtoclub.dn.ua/exam");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @SuppressWarnings("deprecation") @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @TargetApi(Build.VERSION_CODES.N) @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        };
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("file:///android_asset/www/index.html");
 
     }
 }
